02171a6bf7d339d1d041f6f9b0f6dfcc8d64df52
Jingexz01 <64733953+Jingexz01@users.noreply.github.com>
2021-08-14 17:06:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ client.on('ready', () => {
 console.log('UwU Bot is now online!');
 })
 
+bot.on('message', msg=>{
+   if(msg.content === "Test"){
+     msg.reply('Test Command Worked!');
+    }
+})
+
 client.login(process.env.token);
